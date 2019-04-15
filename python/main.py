90270225853e7cba9beb768302297bd8a46c68bb
Tebinski <dangarteb@gmail.com>
2019-04-16 01:47:51 +0200

    i dont thing the class Permu its necessary

--- python/main.py
+++ python/main.py
@@ -14,19 +14,47 @@ from itertools import combinations_with_replacement, permutations
 import numpy as np
 import pandas as pd
 
+class Permu():
+    def __init__(self, bool_info):
+        """ tuple or list as boolean info
+        :example: (1,0,0,1)
+        """
+        self.bool_info = bool_info
+
+    def translate_per(self):
+        """ Given bool notation (1,0,0,1,1) --> [1,2]
+
+          :type permu: list or tuple of integers
+          :param permu: the permutation in boolean notation
+          :return: list
+
+          :example:
+          >>> translate_per([0, 0, 1, 1, 0, 1, 0, 1, 1, 1])
+          >>> [2,1,3]
+          """
+        cnt = 0
+        ls = []
+        for i in self.bool_info:
+            if i:
+                cnt = cnt + 1
+            else:
+                ls.append(cnt)
+                cnt = 0
+        ls.append(cnt)
+        return list(filter((0).__ne__, ls))
 
 # FUNCTIONS DEFINITION
 def translate_per(permu):
     """ Given bool notation (1,0,0,1,1) --> [1,2]
 
-    :type permu: list or tuple of integers
-    :param permu: the permutation in boolean notation
-    :return: list
+      :type permu: list or tuple of integers
+      :param permu: the permutation in boolean notation
+      :return: list
 
-    :example:
-    >>> translate_per([0, 0, 1, 1, 0, 1, 0, 1, 1, 1])
-    >>> [2,1,3]
-    """
+      :example:
+      >>> translate_per([0, 0, 1, 1, 0, 1, 0, 1, 1, 1])
+      >>> [2,1,3]
+      """
     cnt = 0
     ls = []
     for i in permu:
@@ -36,10 +64,9 @@ def translate_per(permu):
             ls.append(cnt)
             cnt = 0
     ls.append(cnt)
-
     return list(filter((0).__ne__, ls))
 
-def gen_per(n, s):
+def gen_elem_per(n, s):
     """ Calculates the permutations for a element of size 'n' in a space of length space 's'
 
     :param: n
@@ -51,7 +78,9 @@ def gen_per(n, s):
     # ls_permu_bad return permutations that not contains element of size n. Lets filter them, translating it to
     # group notation
     ls_permu = [i for i in ls_permu_bad if len(translate_per(i)) == 1]
-    return ls_permu
+    df = pd.DataFrame(ls_permu).drop_duplicates()
+    #todo: it generates equivalent permutations
+    return df
 
 def space_needs(group):
     """ Calculates de min space needed for a given group
@@ -63,21 +92,28 @@ def space_needs(group):
     """
     return np.sum(group) + len(group) - 1
 
-# ToDo: lets generate the obvious values in a possible group permutation
-def get_invariables(elem, space):
-    """ Calculates the invariables in a given group and space
+def gen_group_per(group):
+    """ Given a group of k elements (ei,..., ek), generate possible permutations"""
+    for e in group:
+        ls_ele = gen_elem_per(e, e+1)
+        #todo: ni idea de como generarlo
 
-    :param elem: int
-    :param space: integer
+def get_invariables(n, s):
+    """ Calculates the invariables for a elem(n) and space(s)
+
+    :param n: int
+    :param s: integer
     """
-    for i in elem:
-        pass
+    # ToDo: lets generate the obvious values in a possible group permutation
+
+    df_permu = gen_elem_per(n,s)
+    sr_inv = df_permu.all(axis = 0)
+    return sr_inv
 
 def main():
     """
     Given a row/col of length l, the number of possible combinations of 0 and 1,
     is a combination with replacement.
-
     >>> list(combinations_with_replacement([0, 1], 3))
     >>> [(0, 0, 0),
     >>>  (0, 0, 1),
@@ -85,11 +121,10 @@ def main():
     >>>  (1, 1, 1)]
     
     Remark here that the order doesn't matter, so the combination (1,0,1) is not shown,
-    thus is equivalent as (0,1,1) 
-    
+    thus is equivalent as (0,1,1)
+
     What we need is the list of all permutations.
     Example, given row/col like (0,0,1)
-
     >>> list( permutations([0, 0, 1]) )
     >>> [(0, 0, 1),
     >>>  (0, 1, 0),
@@ -103,24 +138,22 @@ def main():
     So, if we have a element(2) in space(3)
     >>> list(permutations([0, 1, 1]))
 
-
     We use map function to pass the generator to translate_per
     >>> n, s = 2, 4
-    >>> ls_permu = gen_per(n, s)
+    >>> ls_permu = gen_elem_per(n, s)
 
     We can inspect this list of list better if we pass it into pandas
     >>> df_permu = pd.DataFrame(ls_permu).astype(int)
+    >>> ar_permu = np.array(ls_permu)
 
-    # Let be a group with k element of size n
-    # *group notation: (n_i, ..., n_k)
-    #
-    # Thus, gen_per(n,s)
+    Let be a group with k element of size n
+    *group notation: (n_i, ..., n_k)
+    Thus, gen_per(n,s)
 
-    todo : llamar de forma recursiva a gen_per para poder darle como argumento
-    varios grupos, tal que (2,3,1)
+    todo : call gen_permutations with group like (2,3,1)
     
 
-    Vamos a calular la long minima que puede tener un grupo dado
+    Vamos a calcular la long minima que puede tener un grupo dado
 
     Dado un grupo de n_elementos >1, llamamos recursivamente a gen_per hasta 
     que el gr_long_min sea mayor o igual que las celdas vacias.
@@ -138,8 +171,8 @@ def main():
                 while length > i + 1:
                     # no tengo nada claro como montar este bucle
     """
-                 
-
+    get_invariables(3,5)
+    print('end')
 
 if __name__ == '__main__':
     main()
