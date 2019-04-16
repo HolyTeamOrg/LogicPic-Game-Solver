efa4442ee75eb016932f4f2c61012836b23b3dd4
Tebinski <dangarteb@gmail.com>
2019-04-16 20:44:43 +0200

    reorder

--- python/aux_funs.py
+++ python/aux_funs.py
@@ -14,23 +14,24 @@ from itertools import combinations_with_replacement, permutations
 import numpy as np
 import pandas as pd
 
+
+# CLASS DEFINITION
 class Permu():
+    """The Permutation class,
+    #todo: change class name"""
+
     def __init__(self):
         self.bool_info = []
 
-    def def_bool(self, bool_info):
+    def def_bool(self, bool_notation):
         """ tuple or list as boolean info
         :example: (1,0,0,1)
         """
-        self.bool_info = bool_info
+        self.bool_info = bool_notation
 
     @property
     def group(self):
-        """ Given bool notation (1,0,0,1,1) --> [1,2]
-
-          :type permu: list or tuple of integers
-          :param permu: the permutation in boolean notation
-          :return: list
+        """ Given bool_notation (1,0,0,1,1) --> [1,2]
 
           :example:
           >>> translate_per([0, 0, 1, 1, 0, 1, 0, 1, 1, 1])
@@ -45,14 +46,15 @@ class Permu():
                 ls.append(cnt)
                 cnt = 0
         ls.append(cnt)
-        return list(filter((0).__ne__, ls))
+        return list( filter( (0).__ne__, ls ) )
+
 
 # FUNCTIONS DEFINITION
-def translate_per(permu):
-    """ Given bool notation (1,0,0,1,1) --> [1,2]
+def translate_per(bool_notation):
+    """ Given bool notation (1,0,..,0,1,1), returns group notation [1,...,2]
 
-      :type permu: list or tuple of integers
-      :param permu: the permutation in boolean notation
+      :type bool_notation: list or tuple of integers
+      :param bool_notation: the permutation in boolean notation
       :return: list
 
       :example:
@@ -61,40 +63,22 @@ def translate_per(permu):
       """
     cnt = 0
     ls = []
-    for i in permu:
+    for i in bool_notation:
         if i:
             cnt = cnt + 1
         else:
             ls.append(cnt)
             cnt = 0
     ls.append(cnt)
-    return list(filter((0).__ne__, ls))
+    return list(filter( (0).__ne__, ls))
 
 
 def trad_min_bool(group):
-    """For a group (gi,...,gk), generates the trad to bool notation
+    """For a group (gi,...,gk), generates the trad to the minimum possibility in bool notation
     :arg: list or tuple
     :return: array """
-    return np.hstack( [1]*g + [0] for g in group )[:-1]
+    return np.hstack([1] * g + [0] for g in group)[:-1]
 
-def gen_shift(group, s):
-    """For a group (gi,...,gk) and a given space s, generates de shift of the group
-    :example:
-    >>> group = (3,1,2)
-    >>> """
-
-    mybool = trad_min_bool(group)
-    min_space = len(mybool)
-    if s < min_space:
-        return group
-    else:
-        first = np.concatenate( (mybool, np.zeros(s - min_space) ) )
-        solution = [first]
-        sol = first
-        while sol[-1] != 1:
-            sol = np.roll(sol,1)
-            solution = solution + [sol]
-        return np.vstack(solution)
 
 def space_needs(group):
     """ Calculates de min space needed for a given group
@@ -108,6 +92,27 @@ def space_needs(group):
     """
     return np.sum(group) + len(group) - 1
 
+
+def gen_shift(group, s):
+    """For a group (gi,...,gk) and a given space s, generates de shift of the group
+    :example:
+    >>> group = (3,1,2)
+    >>> """
+
+    mybool = trad_min_bool(group)
+    min_space = len(mybool)
+    if s < min_space:
+        return group
+    else:
+        first = np.concatenate((mybool, np.zeros(s - min_space)))
+        solution = [first]
+        sol = first
+        while sol[-1] != 1:
+            sol = np.roll(sol, 1)
+            solution = solution + [sol]
+        return np.vstack(solution)
+
+
 def get_invariables(group, s):
     """ Calculates the invariables for a group on a space s
 
@@ -115,5 +120,4 @@ def get_invariables(group, s):
     :param s: integer
     """
     ar_possibles = gen_shift(group, s)
-    return np.all(ar_possibles, axis= 0)
-
+    return np.all(ar_possibles, axis=0)
